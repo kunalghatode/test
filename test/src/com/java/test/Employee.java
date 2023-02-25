c0731d35bc04acfa756a36db3fbcce4e750d3709
Kunal Ghatode <kunalghatode16@gmail.com>
2023-02-25 15:30:27 +0530

    testing

---
+++ test/src/com/java/test/Employee.java
@@ -0,0 +1,21 @@
+package com.java.test;
+
+
+class Student{
+	
+}
+class Student1 extends Student{
+	
+}
+public class Employee{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student e =  new Student1();
+		
+		if(e == null) {
+			
+		}
+	}
+
+}
